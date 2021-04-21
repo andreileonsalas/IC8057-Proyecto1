b1bf648a9e961e32ef8b9cd80b8d4831047c5270
Andrei Rolando León Salas <andreileonsalas@gmail.com>
2021-04-21 15:25:35 -0600

    Cronometro funcionando

--- Proyecto2/index.php
+++ Proyecto2/index.php
@@ -17,7 +17,69 @@
             <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
         <?php echo '<p>Hello World</p>'; ?> 
-        <?php phpinfo(); ?>
+        <?php echo $_SERVER['HTTP_USER_AGENT'];?>
+        <?//php phpinfo(); ?>
+        
+        <?php
+/* example 1 */
+
+for ($i = 1; $i <= 10; $i++) {
+    echo "<br> example 1: " . $i;
+}
+
+/* example 2 */
+
+for ($i = 1; ; $i++) {
+    if ($i > 10) {
+        break;
+    }
+    echo "<br> example 2: " . $i;
+}
+
+/* example 3 */
+
+$i = 1;
+for (; ; ) {
+    if ($i > 10) {
+        break;
+    }
+    echo $i;
+    $i++;
+}
+
+/* example 4 */
+
+for ($i = 1, $j = 0; $i <= 10; $j += $i, print $i, $i++);
+?>
+        
+        <?php
+// Connecting, selecting database
+$dbconn = pg_connect("host=localhost dbname=publishing user=www password=foo")
+    or die('Could not connect: ' . pg_last_error());
+
+// Performing SQL query
+$query = 'SELECT * FROM authors';
+$result = pg_query($query) or die('Query failed: ' . pg_last_error());
+
+// Printing results in HTML
+echo "<table>\n";
+while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
+    echo "\t<tr>\n";
+    foreach ($line as $col_value) {
+        echo "\t\t<td>$col_value</td>\n";
+    }
+    echo "\t</tr>\n";
+}
+echo "</table>\n";
+
+// Free resultset
+pg_free_result($result);
+
+// Closing connection
+pg_close($dbconn);
+?>
+        
+        
         <script src="" async defer></script>
     </body>
 </html>
